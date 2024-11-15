45b6e900d5f49a1abe6083d78b80294cf4f620f3
Ruan Lopes <sruan507@gmail.com>
2024-11-15 17:40:50 -0300

    Unários + Incremento/Decremento + Booleano + Ternário

--- TiposVariaveis/Operadores.java
+++ TiposVariaveis/Operadores.java
@@ -15,7 +15,7 @@ public class Operadores {
         int multiplicacao = 20 * 7; // 140
         int divisao = 15 / 3;   // 5
         int modulo = 18 % 3;    // 0
-        double resultado = (10 * 7) + (20/4);   // 75
+        double total = (10 * 7) + (20/4);   // 75
 
     // Contatenação - junção
         String nomeCompleto = "LINGUAGEM" + "JAVA";
@@ -39,5 +39,54 @@ public class Operadores {
         concatenacao = "1"+(1+1+1);
         System.out.println(concatenacao); // 13 - uma string e o restante numeros em evidencia
 
+    // Unários - anotados no Obesidian
+        int numero = 5;
+        System.out.println(- numero); // -5 -> inverte o valor
+        System.out.println(numero); // 5
+
+        numero = - numero; // número = ele msm de forma negativo - até que haja uma nova iteração
+        System.out.println(numero); // -5
+
+        //numero = + numero; // errado, pois o + nesse caso somente aritmetico
+        numero = numero * 1;
+        System.out.println(numero); // 5 - volta para positivo
+
+    // Incremento
+        int num = 5;
+        // numero = numero ++;
+        num++; // 5++ = 5 + 1
+        System.out.println(num); // 6
+
+        System.out.println(num ++); // 5 // abreviação do valor e depois incrementa
+        System.out.println(num); // 6 // agora fez o incremento
+
+        System.out.println(++ num); // 7 // faz o incremento antes de exibir, assim, deixando "certo" o programa
+
+    // Decremento
+        System.out.println(num --); // 7, ainda não fez o decremento
+        System.out.println(num); // 6, decremento feito
+
+    // Booleanos
+        boolean verdadeiro = true;
+        System.out.println("Inverteu " + !verdadeiro); // false
+        System.out.println(verdadeiro); // true
+
+        verdadeiro = !verdadeiro; // falsa até uma nova iteração - "verdadeiro = false"
+        System.out.println(verdadeiro); // false
+
+    // Ternário "?"
+        int a, b;
+        a = 5;
+        b = 6;
+
+        String resultado = a == b ? "verdadeiro" : "falso";
+        /*
+        String resultado = "";  // inicialmente vazio
+        if( a == b )
+            resultado = "verdadeiro";
+        else
+            resultado = "falso";
+        */
+        System.out.println(resultado); // falso
     }
 }
