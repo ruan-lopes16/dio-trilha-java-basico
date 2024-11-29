866172d6bf6c706b0d3d19141a358e1f5a54d6cb
Ruan Lopes <sruan507@gmail.com>
2024-11-29 10:14:43 -0300

    melhorias no código do desafio

--- desafios/3-contador/Contador.java
+++ desafios/3-contador/Contador.java
@@ -4,23 +4,29 @@ public class Contador {
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
+        boolean sucesso = false;
 
-        // pegando os dois numeros via terminal
-        System.out.print( "Digite o primeiro número: " );
-        int num1 = scanner.nextInt();
+        while (!sucesso){ // enquanto não
+            try {
+                // pegando os dois numeros via terminal
+                System.out.print( "Digite o primeiro número: " );
+                int num1 = scanner.nextInt();
 
-        System.out.print( "Digite o segundo número: " );
-        int num2 = scanner.nextInt();
+                System.out.print( "Digite o segundo número: " );
+                int num2 = scanner.nextInt();
 
-        try {
-            // chamando contagem
-            contar(num1, num2);
+                // chamando contagem
+                contar(num1, num2);
 
-        } catch (ParametrosInvalidosException e) { // exceção
-            // mensagem exceção
-            System.out.println("ERRO: " + e.getMessage());
+                sucesso = true; // se não ocorrer exceção, marcar como sucesso
 
+            } catch (ParametrosInvalidosException e) { // exceção
+                // mensagem exceção
+                System.out.println("ERRO: " + e.getMessage());
+
+            }
         }
+        scanner.close(); // fechando scanner após o uso
     }
 
     static void contar(int num1, int num2) throws ParametrosInvalidosException {
@@ -31,6 +37,8 @@ public class Contador {
         if (num1 < 0 || num2 < 0) { // em caso de num1 ou num2 negativos
             throw new ParametrosInvalidosException("Os números não podem ser negativos");
         }
+
+        System.out.println("Segue contagem:");
         // criando loop para de interações de num1 até num2
         for (int count = num1; count <= num2; count++) {
             System.out.println(count);
