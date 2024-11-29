8ddcef412cf8800f012036799fd2ccc3f35905e5
Ruan Lopes <sruan507@gmail.com>
2024-11-28 23:04:45 -0300

    desafio: Contador

---
+++ desafios/3 - contador/Contador.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+
+public class Contador {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        // pegando os dois numeros via terminal
+        System.out.print( "Digite o primeiro número: " );
+        int num1 = scanner.nextInt();
+
+        System.out.print( "Digite o segundo número: " );
+        int num2 = scanner.nextInt();
+
+        try {
+            // chamando contagem
+            contar(num1, num2);
+
+        } catch (ParametrosInvalidosException e) {
+            // mensagem exceção
+            System.out.println("ERRO: " + e.getMessage());
+
+        }
+    }
+
+    static void contar(int num1, int num2) throws ParametrosInvalidosException {
+
+        if ( num1 > num2 ) { // em caso de num2 maior que num1
+            throw new ParametrosInvalidosException("O segundo deve ser maior que o primeiro");
+        }
+        if (num1 < 0 || num2 < 0) { // em caso de num1 ou num2 negativos
+            throw new ParametrosInvalidosException("Os números não podem ser negativos");
+        }
+        // criando loop para de interações de num1 até num2
+        for (int count = num1; count <= num2; count++) {
+            System.out.println(count);
+
+        }
+    }
+}
